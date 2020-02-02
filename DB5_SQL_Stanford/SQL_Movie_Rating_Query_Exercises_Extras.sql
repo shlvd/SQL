2226eb9230fef2435d3ce49c3ee0a97b8043fb87
Den <39949334+shlvd@users.noreply.github.com>
2020-02-02 18:54:40 +0200

    Add files via upload

--- DB5_SQL_Stanford/SQL_Movie_Rating_Query_Exercises_Extras.sql
+++ DB5_SQL_Stanford/SQL_Movie_Rating_Query_Exercises_Extras.sql
@@ -54,3 +54,6 @@ group by mID
 order by average desc, title;
 
 --8. Find the names of all reviewers who have contributed three or more ratings. (As an extra challenge, try writing the query without HAVING or without COUNT.)
+SELECT name
+FROM Reviewer
+WHERE (SELECT COUNT(*) FROM Rating WHERE Rating.rId = Reviewer.rId) >= 3;
